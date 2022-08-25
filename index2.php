c17f72e1228e8706f77b887bb0b4b721ff4da7c6
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-08-25 00:49:31 -0300

    Primeiro Commit

---
+++ index2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Página 2</title>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+  <style type="text/css">
+    .linha {
+      font-weight: bold;
+      color: green;
+      padding-left: 10px;
+      line-height: 50px;
+    }
+  </style>
+</head>
+
+<body>
+  <?php
+  for ($i = 0; $i < 10; $i++) {
+    print("<span class=\"linha\">Linha número " . $i . "</span><br />");
+  }
+  ?>
+</body>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    alert("Woool!");
+  });
+</script>
+
+</html>
